aaa946dcac97ecfadcd25681b98b6f44b32993e3
mengyao36 <mengyao.hu@stonybrook.edu>
2022-10-25 15:23:48 -0400

    update

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -18,13 +18,7 @@ connection_string_gcp = f'mysql+pymysql://{GCP_MYSQL_USER}:{GCP_MYSQL_PASSWORD}@
 db_gcp = create_engine(connection_string_gcp)
 
 # show tables from databases
-tableNames_gcp = db_gcp.table_names()
-
-# reorder tables based on what will be the parent table vs. child table
-tableNames_gcp = ['production_patient_conditions', 'production_patient_medications', 'production_medications', 'production_patients', 'production_conditions']
-
-# delete everything
-droppingFunction_all(tableNames_gcp, db_gcp)
+tableNames_gcp = db_gcp.table_names
 
 #  drop the old tables that do not start with production_
 def droppingFunction_limited(dbList, db_source):
@@ -42,3 +36,83 @@ def droppingFunction_all(dbList, db_source):
         print(f'dropped table {table} succesfully!')
     else:
         print(f'kept table {table}')
+
+
+# create tables
+# note: if use MySQL Workbench, in Functions, need to add "use patient_portal;" before all the commands to run (select database first)
+
+table_prod_patients = """
+create table if not exists production_patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    age varchar(255) default null,
+    gender varchar(255) default null,
+    race varchar(255) default null,
+	insurance_type varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id)
+); 
+"""
+
+table_prod_medications = """
+create table if not exists production_medications (
+    id int auto_increment,
+    med_ndc varchar(255) default null unique,
+    med_human_name varchar(255) default null,
+    med_is_dangerous varchar(255) default null,
+    PRIMARY KEY (id)
+); 
+"""
+
+table_prod_conditions = """
+create table if not exists production_conditions (
+    id int auto_increment,
+    icd10_code varchar(255) default null unique,
+    icd10_description varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_prod_patients_medications = """
+create table if not exists production_patient_medications (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    med_ndc varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (med_ndc) REFERENCES production_medications(med_ndc) ON DELETE CASCADE
+); 
+"""
+
+table_prod_patient_conditions = """
+create table if not exists production_patient_conditions (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    icd10_code varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES production_patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (icd10_code) REFERENCES production_conditions(icd10_code) ON DELETE CASCADE
+); 
+"""
+
+# execute the commands above to create tables
+db_gcp.execute(table_prod_patients)
+db_gcp.execute(table_prod_medications)
+db_gcp.execute(table_prod_conditions)
+db_gcp.execute(table_prod_patients_medications)
+db_gcp.execute(table_prod_patient_conditions)
+
+
+# show tables from databases
+gcp_tables = db_gcp.table_names()
+
+# reorder tables based on what will be the parent table vs. child table
+tableNames_gcp = ['production_patient_conditions', 'production_patient_medications', 'production_medications', 'production_patients', 'production_conditions']
+
+# delete everything
+droppingFunction_all(tableNames_gcp, db_gcp)
