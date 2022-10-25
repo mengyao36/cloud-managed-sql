4cdd686c9e0663b3389b40263a7a448c98ad6126
mengyao36 <mengyao.hu@stonybrook.edu>
2022-10-25 13:55:21 -0400

    update code

--- sql_table_creation.py
+++ sql_table_creation.py
@@ -5,3 +5,40 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
+GCP_MYSQL_HOSTNAME = os.getenv("GCP_MYSQL_HOSTNAME")
+GCP_MYSQL_USER = os.getenv("GCP_MYSQL_USERNAME")
+GCP_MYSQL_PASSWORD = os.getenv("GCP_MYSQL_PASSWORD")
+GCP_MYSQL_DATABASE = os.getenv("GCP_MYSQL_DATABASE")
+
+# create connection string to GCP called db_GCP
+connection_string_gcp = f'mysql+pymysql://{GCP_MYSQL_USER}:{GCP_MYSQL_PASSWORD}@{GCP_MYSQL_HOSTNAME}:3306/{GCP_MYSQL_DATABASE}'
+db_gcp = create_engine(connection_string_gcp)
+
+# show tables from databases
+tableNames_gcp = db_gcp.table_names()
+
+# reorder tables based on what will be the parent table vs. child table
+tableNames_gcp = ['production_patient_conditions', 'production_patient_medications', 'production_medications', 'production_patients', 'production_conditions']
+
+# delete everything
+droppingFunction_all(tableNames_gcp, db_gcp)
+
+#  drop the old tables that do not start with production_
+def droppingFunction_limited(dbList, db_source):
+    for table in dbList:
+        if table.startswith('production_') == False:
+            db_source.execute(f'drop table {table}')
+            print(f'dropped table {table}')
+        else:
+            print(f'kept table {table}')
+
+# drop all tables
+def droppingFunction_all(dbList, db_source):
+    for table in dbList:
+        db_source.execute(f'drop table {table}')
+        print(f'dropped table {table} succesfully!')
+    else:
+        print(f'kept table {table}')
